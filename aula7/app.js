2fdd74545a7527b9a085cd984bb841a185537639
Rafael Cunha <51427048+rocunha09@users.noreply.github.com>
2021-03-25 20:21:57 -0300

    cadastrando e listando despesas usando sequelize+handlebars+bodyparser+moment.

--- aula7/app.js
+++ aula7/app.js
@@ -2,16 +2,26 @@ const express = require('express');
 const app = express();
 const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
-const pagamento = require('./models/pagamento');
+const moment = require('moment');
+const Pagamento = require('./models/pagamento');
 
-app.engine('handlebars', handlebars({defaultlayout: 'main'}));
+app.engine('handlebars', handlebars({
+    defaultlayout: 'main',
+    helpers: {
+        formatDate: (date)=>{
+            return moment(date).format('DD/MM/YYYY')
+        }
+    }
+}));
 app.set('view engine', 'handlebars');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.get("/pagamento", function(req, res){
-    res.render("pagamento");
+    Pagamento.findAll({order: [['id', 'DESC']]}).then(function(pagamentos){
+        res.render('pagamento', {pagamentos: pagamentos});
+    })
 });
 
 app.get("/cad-pagamento", function(req, res){
@@ -22,7 +32,7 @@ app.post("/add-pagamento", function(req, res){
     //teste de recebimento de dados
     //res.send("Nome: "+ req.body.nome + "<br>Valor: "+ req.body.valor);
     
-    pagamento.create({
+    Pagamento.create({
         nome: req.body.nome,
         valor: req.body.valor
     }).then(function(){
